8dc09c017108a31660c32b5b0cb23992f162ecda
Pierre Crespi <Pierre@imac-de-pierre-crespi.local>
2018-11-16 15:19:17 +0100

    Initial Commit

---
+++ Multipages/DataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DataViewController.swift
+//  Multipages
+//
+//  Created by  Pierre Crespi on 12/08/2018.
+//  Copyright © 2018  Pierre Crespi. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("DataViewController.viewDidLoad")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
